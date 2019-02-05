446cec608bbc581ea62377316975de02f743a224
Mads R. B. Kristensen <madsbk@gmail.com>
2019-02-05 14:40:40 +0100

    clean up and version bump

--- gcc7/build_gcc7.sh
+++ gcc7/build_gcc7.sh
@@ -3,18 +3,27 @@
 
 set -e
 set -x
-cd ~/
 
 # Install gcc7 (/opt/gcc7)
-wget ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-7.2.0/gcc-7.2.0.tar.gz
-tar -xzf gcc-7.2.0.tar.gz
-cd gcc-7.2.0
-pwd
-./contrib/download_prerequisites
-mkdir -p ~/gcc_build
+tar -xf deps/gcc-7.3.0.tar.xz
+mv gcc-7.3.0 ~/gcc_src
+mv deps/* ~/gcc_src/
+cd ~/gcc_src
+./contrib/download_prerequisites --no-graphite
+mkdir ~/gcc_build
 cd ~/gcc_build
-~/gcc-7.2.0/configure --prefix ~/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls
-make 2> ~/gcc_build.err || true
-tail ~/gcc_build.err
+#~/gcc_src/configure --prefix ~/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls --disable-lto --disable-multilib
+~/gcc_src/configure --prefix ~/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls
+make &> log.txt # travis cannot handle that much output
 make install
+cd ~/gcc7_install/
+#rm -R share
+#find . -name "*.a" -type f|xargs rm -f
+
+# Stripping unneeded data in the binaries
+find ./libexec/gcc/x86_64-pc-linux-gnu/7.3.0 -maxdepth 1 -type f -size +10M -print0 | xargs -0 \
+    strip --strip-unneeded --remove-section=.comment --remove-section=.note
+
+ls -Rlh
+du -sh
 
